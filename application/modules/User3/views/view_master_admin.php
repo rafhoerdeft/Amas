644b124217dd2534781a99bdb75a041a03f7ca66
Rafho <44487637+rafhoerdeft@users.noreply.github.com>
2021-09-28 10:41:29 +0700

    Delete application/modules/User3 directory
    delete user3

--- application/modules/User3/views/view_master_admin.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    $this->load->view('komponen/header',$header);
-    $this->load->view('menu',$menu);
-    if(isset($konten))$this->load->view($konten, $cont);
-    $this->load->view('komponen/footer',$footer);
-?>
