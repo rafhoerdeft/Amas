93961369c908008deaf138164437c3a2a30b87b1
Rafho <44487637+rafhoerdeft@users.noreply.github.com>
2022-10-04 13:02:06 +0700

    Delete application/helpers directory
    Delete

--- application/helpers/csrftoken_helper.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	if (!function_exists('token_csrf')) {
-		function token_csrf(){
-            $CI = & get_instance();
-			$token =  "<input type='hidden' name='".$CI->security->get_csrf_token_name()."' value='".$CI->security->get_csrf_hash()."'>";
-		    return $token;
-		}
-	}
-?>
